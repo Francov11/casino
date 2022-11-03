f7f1c46271f6baf1bdcb8dbded2364c1275cc312
francovelazquez2011 <francovelazquez2011@gmail.com>
2022-11-02 23:36:17 -0300

    Blackajack

--- src/games/blackjag/classBlackJag.ts
+++ src/games/blackjag/classBlackJag.ts
@@ -37,7 +37,79 @@ export class BlackJag {
         let cartas:number[] = [1,2,3,4,5,6,7,8,9,10,10,10,10]
         let a = Math.floor(Math.random() * cartas.length)       
         let b = Math.floor(Math.random() * cartas.length)
-        
-        
+        let aux: number = a + b
+        console.log('Primera carta: ' + a + ' Segunda carta: ' + b)
+        if(aux < 21){
+            let c = Math.floor(Math.random() * cartas.length)
+            aux = a + b + c
+            console.log('Nueva carta: ' + c )
+        } else if(aux > 21 ){
+            console.log('Suma: ' + aux)
+            console.log('Perdiste')
+        } else if(aux === 21){
+            console.log('Suma: ' + aux)
+            console.log('Ganaste')
+        }
+    }
+    public playBlackJag2(){
+        let cartas:number[] = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+        let a = Math.floor(Math.random() * cartas.length)       
+        let b = Math.floor(Math.random() * cartas.length)
+        let aux: number = a + b
+        console.log('Primera carta: ' + a + ' Segunda carta: ' + b)
+        while(aux !== 21 ){
+            let x = Math.floor(Math.random() * cartas.length)
+            aux = a + b + x
+            console.log('Nueva carta: ' + x + ' Suma: ' + aux)
+        }
+        console.log('Suma final: ' + aux)
+     
+    }
+    public playBlackJag3(){
+        let cartas:number[] = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+        let a = Math.floor(Math.random() * cartas.length)       
+        let b = Math.floor(Math.random() * cartas.length)
+        let aux: number = a + b
+        console.log('Primera carta: ' + a + ' Segunda carta: ' + b)
+        do{
+            let x = Math.floor(Math.random() * cartas.length)
+            aux = a + b + x
+            console.log('Nueva carta: ' + x + ' Suma: ' + aux)
+            if(aux > 21){
+                console.log('Perdiste, sumas: ' + aux)
+                break;
+            }
+        } while(aux < 21 || aux > 21)
+        if(aux === 21){
+            console.log('Ganaste, sumas: ' + aux)
+        }
+    }
+
+    public playBlackjack() {
+        let cartas:number[] = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+        let a = Math.floor(Math.random() * cartas.length)       
+        let b = Math.floor(Math.random() * cartas.length)
+        let aux: number = a + b
+        let option: number = 1 // Aca utlizaria el readline-sink
+        switch(option){ //Utilizar switch dentro del while para poder pedir una carta si suma menos de 21?
+            case 1: {
+                console.log('Hit')
+                let x = Math.floor(Math.random() * cartas.length)
+                aux = a + b + x
+                if(aux === 21){
+                    console.log('Ganaste, sumas: ' + aux)
+                }
+            }
+                break;
+            case 2: {
+                console.log('Drop')
+                console.log('You lose')
+            }
+                break;
+        }
     }
 }
+
+let tableOne = new BlackJag(1, 2, true)
+
+tableOne.playBlackjack()
